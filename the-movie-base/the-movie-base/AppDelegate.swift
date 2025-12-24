8d144e094346d96dc09965f80687ffd627f30fd2
theln54 <lengocthe54@gmail.com>
2025-12-24 10:53:11 +0700

    init project

---
+++ the-movie-base/the-movie-base/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  the-movie-base
+//
+//  Created by lnthe on 24/12/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
